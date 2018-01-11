425d4369d14fe004c8111cec4e99930f44ee1078
Nicolas Kovacs <info@microlinux.fr>
2018-01-11 10:50:29 +0100

    Mise à jour initiale du système.

---
+++ postinstall.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# postinstall.sh
+#
+# Script post-installation pour CentOS 7 + Xfce
+#
+# (c) Nicolas Kovacs, 2018
+
+# Exécuter en tant que root
+if [ $EUID -ne 0 ] ; then
+  echo "::"
+  echo ":: Vous devez être root pour exécuter ce script."
+  echo "::"
+  exit 1
+fi
+
+# Répertoire courant
+CWD=$(pwd)
+
+# Interrompre en cas d'erreur
+set -e
+
+# Couleurs
+VERT="\033[01;32m"
+GRIS="\033[00m"
+
+# Journal
+LOG=/tmp/postinstall.log
+
+# Pause entre les opérations
+DELAY=1
+
+# Nettoyer le fichier journal
+echo > $LOG
+
+# Bannière
+sleep $DELAY
+echo
+echo "     #######################################" | tee -a $LOG
+echo "     ### CentOS 7 Xfce Post-installation ###" | tee -a $LOG
+echo "     #######################################" | tee -a $LOG
+echo | tee -a $LOG
+sleep $DELAY
+echo "     Pour suivre l'avancement des opérations, ouvrez une"
+echo "     deuxième console et invoquez la commande suivante :"
+echo
+echo "       # tail -f /tmp/postinstall.log"
+echo
+sleep $DELAY
+
+# Mise à jour initiale
+echo -e ":: Mise à jour initiale du système... \c"
+yum -y update >> $LOG 2>&1
+echo -e "[${VERT}OK${GRIS}] \c"
+sleep $DELAY
+echo
+
